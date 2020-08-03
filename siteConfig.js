2536f31321f279eec98cabfff7b269bd589b284f
Tarek Maghin <ahmedshabana0.0@gmail.com>
2020-08-04 00:38:18 +0200

    test commit

--- siteConfig.js
+++ siteConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   shortName: "maghin",
   description:
     "Having built many personal projects and client websites, Maghin became a great web designer and developer who is able to turn any idea into an amazing experience. Here are the Case Study's",
-  url: "https://maghin-portfolio-one.netlify.com", // Domain of your site without prefix!
+  url: "https://maghin.netlify.app/", // Domain of your site without prefix!
   prefix: "/",
   author: "Maghin",
   twitter: "",
